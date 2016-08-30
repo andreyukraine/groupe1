795824bdeacd8d765e963d96a8050ec2039183e1
countess88 <countess88@mail.ru>
2016-08-30 15:18:54 +0300

    Lada, added package exception, 1 exception, changes in AppDBImpl

--- src/ua/artcode/db/AppDBImpl.java
+++ src/ua/artcode/db/AppDBImpl.java
@@ -1,45 +1,85 @@
 package ua.artcode.db;
 
-import ua.artcode.model.Company;
-import ua.artcode.model.Moderator;
-import ua.artcode.model.Service;
-import ua.artcode.model.User;
+import ua.artcode.exceptions.ServiceNotUpdateException;
+import ua.artcode.model.*;
 
-import static ua.artcode.test.controller.IModeratorPSAControllerTest.services;
+import java.util.List;
 
 /**
  * Created by serhii on 21.08.16.
  */
 public class AppDBImpl implements IAppDB {
 
-    private Company[] companies;
-    private User[] users;
-    private Moderator[] moderator;
-    private Service[] services1 = new Service[10];
+    private List<Company> companies;
+    private List<Client> clients;
+    private List<Moderator> moderator;
+    private List<Service> services;
 
 
     public Service addService(Service service) {
-        for (int i = 0; i < services1.length; i++) {
-            if (services1[i] == null) {
-                services1[i] = service;
-                return service;
-            }
-        }
-        return null;
+        services.add(service);
+        return service;
     }
 
     public Service updateService(long serviceId, Service service){
-        for (int i = 0; i < services1.length; i++) {
-            if (services1[i].getId() == serviceId){
-                services1[i] = service;
-                return service;
+
+            for (int i = 0; i < services.size(); i++) {
+                if (services.get(i).getId() == serviceId){
+                    services.set(i, service);
+                    return service;
+                }
             }
+        try {
+            throw new ServiceNotUpdateException("Servise doesn't exist");
+        } catch (ServiceNotUpdateException e) {
+            e.printStackTrace();
         }
-        return null;
+        return null; //надо как-то правильно добавить exception, чтоб убрать return null;
     }
+
     // constructors --------------------------------------------------------------------------------
 
+    public AppDBImpl() {
+    }
+
+    public AppDBImpl(List<Company> companies, List<Client> clients, List<Moderator> moderator, List<Service> services) {
+        this.companies = companies;
+        this.clients = clients;
+        this.moderator = moderator;
+        this.services = services;
+    }
     // geters & seters -----------------------------------------------------------------------------
 
 
+    public List<Company> getCompanies() {
+        return companies;
+    }
+
+    public void setCompanies(List<Company> companies) {
+        this.companies = companies;
+    }
+
+    public List<Client> getClients() {
+        return clients;
+    }
+
+    public void setClients(List<Client> clients) {
+        this.clients = clients;
+    }
+
+    public List<Moderator> getModerator() {
+        return moderator;
+    }
+
+    public void setModerator(List<Moderator> moderator) {
+        this.moderator = moderator;
+    }
+
+    public List<Service> getServices() {
+        return services;
+    }
+
+    public void setServices(List<Service> services) {
+        this.services = services;
+    }
 }
---
+++ src/ua/artcode/exceptions/ServiceNotUpdateException.java
@@ -0,0 +1,12 @@
+package ua.artcode.exceptions;
+
+/**
+ * Created by LadaP on 8/30/2016.
+ */
+public class ServiceNotUpdateException extends Exception{
+
+    public ServiceNotUpdateException (String message) {
+        System.out.println(message);
+    }
+
+}
